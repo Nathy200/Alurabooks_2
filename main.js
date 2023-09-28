04c3cc9a6b9f666e2571b543af3d71071958144b
Nathaly Dias <112184960+Nathy200@users.noreply.github.com>
2023-09-28 20:01:38 -0300

    .

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-funetion tocaSomPom (){
+function tocaSomPom (){
     document.querySelector(`#som_tecla_pom`).play():
 }
-document.querySelector(`.tecla_pom`);
+document.querySelector(`.tecla_pom`).onclick tocaSomPom;
+function tocaSomPom(){
+    document.querySelection(`#Som_tecla_Clap`).play();
+    document.querySelector(`.tecla_pom`)
+    onclick tocaSomPom;
+}
